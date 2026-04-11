e90125b6d952b97d071b7c1e3e87ad6fbb9bf2d3
hapo-nghialuu <nghialt@haposoft.com>
2026-04-11 15:52:52 +0700

    fix(spec): use platform.folder for references dest path

--- packages/spec/bin/install.js
+++ packages/spec/bin/install.js
@@ -543,7 +543,7 @@ function copyPlatformFiles(platformKey, results, options = {}) {
       // Copy agent reference manuals (debugger manuals, etc.) recursively
       const refsSource = path.join(__dirname, '../src', platform.sourceDir, 'references');
       if (fs.existsSync(refsSource)) {
-        const refsDest = path.join(platform.destDir, 'references');
+        const refsDest = path.join(platform.folder, 'references');
         const refsExisted = fs.existsSync(refsDest);
         copyRecursive(refsSource, refsDest, options);
 
